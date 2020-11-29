ddb9099692040d1bd951094bdd948831aa387649
unknown <walkerdp357@gmail.com>
2020-11-29 17:17:55 -0500

    bg fix 2

--- bgdeploy.sh
+++ bgdeploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Description:
-#   This script performs a blue-green deployment into prod 
+#   This script performs a blue-green deployment into prod
 #   environment using ansible.
 #
 # Source: None
@@ -9,8 +9,8 @@
 
 ans="/home/wprice/ansible"
 inactive=""
-bg=["blue", "green"]
+bg=("blue" "green")
 
 sshpass -p "Tfsadmin13" ssh -o StrictHostKeyChecking=no wprice@192.168.0.115 test -f /home/wprice/green/active 
-inactive=bg[$?]
+inactive=${bg[$?]}
 ansible-playbook -i $ans/prod-$inactive $ans/deploy.yml
